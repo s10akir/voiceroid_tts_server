836597539808a27aa019b16c3fbd096cbe7ccd59
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2022-01-03 21:59:53 +0900

    feat: add black

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from pyvcroid2 import VcRoid2
 app = FastAPI()
 vc = VcRoid2()
 
+
 @app.get("/")
 async def root():
     languages = vc.listLanguages()
     voices = vc.listVoices()
 
-    return {
-        "languages": languages,
-        "voices": voices
-    }
+    return {"languages": languages, "voices": voices}

3812cb6308871cffc8d56412b08d9168862f4f07
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2022-01-03 21:22:05 +0900

    feat: create api server

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pyvcroid2 import VcRoid2
+
+app = FastAPI()
+vc = VcRoid2()
+
+@app.get("/")
+async def root():
+    languages = vc.listLanguages()
+    voices = vc.listVoices()
+
+    return {
+        "languages": languages,
+        "voices": voices
+    }
